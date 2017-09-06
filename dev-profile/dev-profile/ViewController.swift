7c8cfce07a8551f897fb5590900546637b957108
hasanakoglu <a_hasan123@hotmail.co.uk>
2017-09-06 10:49:55 +0100

    autolayout and size classes

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by CICD2 on 05/09/2017.
+//  Copyright © 2017 CICD2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
